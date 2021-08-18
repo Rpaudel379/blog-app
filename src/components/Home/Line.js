62a03a9978e6522234dfe1278eb199f2bc31cc4b
rpaudel379 <rpaudel379@gmail.com>
2021-08-18 12:39:47 +0530

    fixed login page on homepage

--- src/components/Home/Line.js
+++ src/components/Home/Line.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useGlobalContext } from "../../context/context";
 import Add from "../Home/Add";
-import Login from "../../pages/Login";
+import Login from "./Login";
 const Line = () => {
   const { userData } = useGlobalContext();
 
